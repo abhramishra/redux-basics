1017d62ed8fc82ef2a7125dbdfbcf0c8154a6c57
YOUR NAME <abhra.mishra@kloctechnologies.co.in>
2020-03-30 18:01:02 +0530

    Added employee in redux store - CRUD operation

--- dct-redux/redux.js
+++ dct-redux/redux.js
@@ -17,7 +17,6 @@ const setTo = (n) => {
 
 // reducers are regular functions
 const countReducer = (state = 0, action) => {
-    console.log('Inside Reducer, state - ', state)
     switch(action.type) {
         case 'INCREMENT': {
             return state + 1
@@ -26,7 +25,7 @@ const countReducer = (state = 0, action) => {
             return state - 1
         }
         case 'INCREMENT_BY': {
-            return state += action.payload
+            return state + action.payload
         }
         case 'SET_TO': {
             return state = action.payload
@@ -37,28 +36,82 @@ const countReducer = (state = 0, action) => {
     }
 }
 
+const addEmployee = (emp) => {
+    return { type: 'ADD_EMPLOYEE', payload: emp}
+}
+const removeEmployee = (empId) => {
+    return { type: 'REMOVE_EMPLOYEE', payload: empId }
+}
+const updateEmployee = (empId, obj) => {
+    return { type: 'UPDATE_EMPLOYEE', payload: {
+        empId,
+        obj
+    } }
+}
+
+employeesInitialState = []
+const employeesReducer = (state = employeesInitialState, action) => {
+    switch(action.type) {
+        case 'ADD_EMPLOYEE': {
+            return state.concat(action.payload)
+        }
+        case 'UPDATE_EMPLOYEE': {
+            return state.map(emp => {
+                if(emp.id == action.payload.empId) {
+                    return Object.assign(emp, action.payload.obj)
+                } else {
+                    return Object.assign({}, emp)
+                }
+            })
+        }
+        case 'REMOVE_EMPLOYEE': {
+            return state.filter(emp => emp.id != action.payload)
+        }
+        default: {
+            return state
+        }
+    }
+}
+
 const configureStore = () => {
-    console.log('Inside store')
     const store = createStore(combineReducers({
-        count: countReducer
+        count: countReducer,
+        employees: employeesReducer
     }))
     return store
 }
+
 const store = configureStore()
-console.log(store.getState())
+// console.log(store.getState())
+
+// subscribe method will call whenever state will update
+store.subscribe(() => {
+    console.log(store.getState())
+})
+/*
+store.dispatch(increment())
 
 store.dispatch(increment())
-console.log(store.getState())
-
-store.dispatch(increment())
-console.log(store.getState())
 
 store.dispatch(decrement())
-console.log(store.getState())
 
 store.dispatch(incrementBy(5))
-console.log(store.getState())
 
 store.dispatch(setTo(10))
-console.log(store.getState())
+*/
+store.dispatch(addEmployee({ id: 1, name: 'Employee1' }))
+store.dispatch(addEmployee({ id: 2, name: 'Employee2' }))
+
+// list all the employees
+console.log('List employees - ',store.getState().employees)
+
+// show particular employee
+console.log('Employee Show - ', store.getState().employees.find(emp => emp.id == 1))
+
+// update employee
+store.dispatch(updateEmployee(1,{ id: 1, name: 'Abhra' }))
+
+// remove employee
+store.dispatch(removeEmployee(2))
+console.log('List employees - ',store.getState().employees)
 
